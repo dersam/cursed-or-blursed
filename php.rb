61f4e9d75ef39ff545238788cecfc2d09d5b15e9
Sam Schmidt <sam.schmidt@shopify.com>
2026-08-19 13:50:36 -0400

    rebind block into function so return works

--- php.rb
+++ php.rb
@@ -1,24 +1,39 @@
 module PHP
+  @last = global_variables.dup
+  class << self; attr_accessor :last; end
+
   refine Object do
-    def echo(*parts) = (print parts.join; print "\n") # echo "a" + "b";
+    def echo(*parts) = (print parts.join; print "\n"; PHP.last = global_variables.dup) # echo "a", "b";
     def array(*items) = items                         # array(1, 2, 3)
     def isset(x) = !x.nil?                             # isset($x)
     def count(x) = x.size                              # count($x)
     def strlen(s) = s.length
-    def implode(glue, xs) = xs.join(glue)             # implode(", ", $x)
+    def implode(glue, xs) = (PHP.last = global_variables.dup; xs.join(glue)) # implode(", ", $x)
 
-    def function(input)
-      define_singleton_method(input[0]) { |*args, **kwargs| input[1].call(*args, **kwargs) }
+    def function(input)                               # function greet($who) { return ... }
+      name, blk, params = input
+      define_singleton_method(:__fn_, &blk)           # block becomes a Method so `return` scopes to it
+      fn = method(:__fn_)
+      define_singleton_method(name) do |*args, **kwargs|
+        params&.each_with_index { |p, i| eval("#{p} = args[i]", binding) if args[i] }
+        fn.call
+      end
+      PHP.last = global_variables.dup
     end
   end
 end
 
 using PHP
+null = nil
 
 def method_missing(name, *args, **kwargs, &block)
   return nil if %i(to_a to_hash to_io to_str to_ary to_int).include?(name)
 
-  [name, block]
+  if block                                            # function greet($who) { ... }
+    [name, block, global_variables - PHP.last]
+  else
+    [name, block]
+  end
 end
 
 # ===== begin "PHP" =======
@@ -36,5 +51,6 @@ function greet($who) {
 
 echo greet("world");
 echo implode(" ", ["strlen(name) =", strlen($name)]);
+echo isset(null);
 
 # ===== end "PHP" =======
